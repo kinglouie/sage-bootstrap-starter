502d38aa99f720edbe32f7b5b9a5ce7b08805bab
Matthias Hollerbach <mail@matthias-hollerbach.de>
2023-10-23 13:05:05 +0200

    add bs container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,23 @@
 
 @include('sections.header')
 
+<div class="container-xxl">
+  @hasSection('sidebar')
+  <div class="row">
+    <div class="col-lg-9">
+  @endif
   <main id="main" class="main">
     @yield('content')
   </main>
-
   @hasSection('sidebar')
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
+    </div>
+    <div class="col-lg-3">
+      <aside class="sidebar">
+        @yield('sidebar')
+      </aside>
+    </div>
+  </div>
   @endif
+</div>
 
 @include('sections.footer')
--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,5 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer class="footer-default bg-light bg-gradient py-4">
+  <div class="container-xxl">
+    @php(dynamic_sidebar('sidebar-footer'))
+  </div>
 </footer>
