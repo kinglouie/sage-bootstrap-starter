43ce44844aa0279944805837abbf8b091372c6c7
Matthias Hollerbach <mail@matthias-hollerbach.de>
2023-10-23 13:26:55 +0200

    inject bootstrap scss vars into Gutenberg Plugin

--- app/filters.php
+++ app/filters.php
@@ -14,3 +14,21 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+add_filter( 'areoi-overwrite-vars', function( $var_content ) {
+
+	$var_content .= '
+		$spacers: (
+		  0: 0,
+		  1: $spacer * .25,
+		  2: $spacer * .5,
+		  3: $spacer,
+		  4: $spacer * 1.5,
+		  5: $spacer * 3,
+		  6: $spacer * 4,
+		  7: $spacer * 5,
+		);
+	';
+    $customs_vars = file_get_contents(get_template_directory() .'/resources/styles/_variables.scss');
+	return $var_content . $customs_vars;
+} );
