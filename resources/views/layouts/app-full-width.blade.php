a802e3a39f50909256cc9ebede661eb8d1266f84
Matthias Hollerbach <mail@matthias-hollerbach.de>
2023-10-23 13:04:38 +0200

    add base page templates

---
+++ resources/views/layouts/app-full-width.blade.php
@@ -0,0 +1,17 @@
+<a class="visually-hidden visually-hidden-focusable" href="#main">
+  {{ __('Skip to content') }}
+</a>
+
+@include('sections.header')
+
+  <main id="main" class="main">
+    @yield('content')
+  </main>
+
+  @hasSection('sidebar')
+    <aside class="sidebar">
+      @yield('sidebar')
+    </aside>
+  @endif
+
+@include('sections.footer')
